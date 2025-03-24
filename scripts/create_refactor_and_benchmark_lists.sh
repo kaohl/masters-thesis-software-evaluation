b31ba1f552a27e4ff12ec4d3da273dbeeffba7b0
Adam Ohlsson <karladamohlsson@gmail.com>
2025-03-24 18:20:52 +0100

    Update script

--- scripts/create_refactor_and_benchmark_lists.sh
+++ scripts/create_refactor_and_benchmark_lists.sh
@@ -4,6 +4,14 @@ data=$1 # Experiments
 xp=$2 # Experiment
 bm=$3 # Benchmark
 
+# Here we must use a runtime capable of running all benchmarks.
+# Because, we need to run the benchmark to generate steering
+# for the refactoring framework when setting up the eclipse
+# workspace. When benchmarking, the JDK/JRE is set internally
+# based on experimental parameters instead, ignoring this sdk.
+
+sdk use java 17.0.14-tem
+
 ./evaluation.py --data $data --xs $xp --bs $bm --create
 ./evaluation.py --data $data --xs $xp --bs $bm --refactor
 n=`./evaluation.py --data $data --xs $xp --bs $bm --show-execution-plan | wc -l`
