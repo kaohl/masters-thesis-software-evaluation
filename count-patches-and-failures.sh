a4aab2746f807f133fcc11745a8e2f410b6b17ba
Adam Ohlsson <karladamohlsson@gmail.com>
2025-04-01 19:34:50 +0200

    Update script printing an overview of experiment data and benchmark progress.

--- count-patches-and-failures.sh
+++ count-patches-and-failures.sh
@@ -1,8 +1,32 @@
 #!/bin/env bash
 
-for bm in `ls experiments/data`; do
-    ok=`find experiments/data/"$bm" -iname patches.txt | wc -l`
-    nk=`find experiments/data/"$bm" -iname FAILURE | wc -l`     # Warning: Includes bm failure as well.
-    echo "Benchmark = $bm, Patches = $ok, Failures = $nk"
+function count_files {
+    find $1 -iname $2 | wc -l
+}
+
+data=experiments/data
+
+for bm in `ls $data`; do
+    location="$data/$bm"
+
+    success_refactor=`count_files $location patches.txt`
+    stats=`count_files $location stats`
+    success_benchmark=`count_files $location SUCCESS`
+    failure=`count_files $location FAILURE`
+    failure_generic=`count_files $location GENERIC`
+    failure_timeout=`count_files $location TIMEOUT`
+    failure_benchmark=$((failure_generic + failure_timeout))
+    failure_refactor=$((failure - failure_benchmark))
+
+    benchmark_patch_coverage="$stats/$success_refactor"
+
+    printf "Benchmark = %-8s, Refactor { success = %-4d, failure = %-4d }, Benchmark { success = %-4d, failure = { generic = %-4d, timeout = %-4d }, patch_coverage = %10s }\n" \
+        "$bm"                       \
+        "$success_refactor"         \
+        "$failure_refactor"         \
+        "$success_benchmark"        \
+        "$failure_generic"          \
+        "$failure_timeout"          \
+        "$benchmark_patch_coverage"
 done
 
