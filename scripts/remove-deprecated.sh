97f8c3153258fab45537131a179542d72ef9a5d9
Adam Ohlsson <karladamohlsson@gmail.com>
2025-04-11 16:09:43 +0200

    Also remove benchmarks

--- scripts/remove-deprecated.sh
+++ scripts/remove-deprecated.sh
@@ -11,8 +11,10 @@ benchmarks=(
 for bm in "${benchmarks[@]}"; do
     ./results.py --x "$bm" --bm "$bm" --show-deprecated
     n=`cat temp/deprecated/refactorings.txt | wc -l`
-    echo "Removing $n deprecated refactorings"
+    m=`cat temp/deprecated/benchmarks.txt | wc -l`
+    echo "Remove deprecated: refactorings = $n, benchmarks = $m"
     rm -rf `cat temp/deprecated/refactorings.txt`
+    rm -rf `cat temp/deprecated/benchmarks.txt`
     rm -rf `find experiments/data/$bm -type d -empty`
 done
 
