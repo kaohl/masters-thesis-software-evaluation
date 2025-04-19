1448747441f9bd5cabbfa6a2aff07adf6a53e544
Adam Ohlsson <karladamohlsson@gmail.com>
2025-04-19 11:39:35 +0200

    Update counting script.

--- count-patches-and-failures.sh
+++ count-patches-and-failures.sh
@@ -9,6 +9,7 @@ data=experiments/data
 for bm in `ls $data`; do
     location="$data/$bm"
 
+    metrics_count=`count_files $location metrics.txt`
     success_refactor=`count_files $location patches.txt`
     stats=`count_files $location stats`
     success_benchmark=`count_files $location SUCCESS`
@@ -20,13 +21,14 @@ for bm in `ls $data`; do
 
     benchmark_patch_coverage="$stats/$success_refactor"
 
-    printf "Benchmark = %-8s, Refactor { success = %-4d, failure = %-4d }, Benchmark { success = %-4d, failure = { generic = %-4d, timeout = %-4d }, patch_coverage = %10s }\n" \
+    printf "Benchmark = %-8s, Refactor { success = %-4d, failure = %-4d }, Benchmark { success = %-4d, failure = { generic = %-4d, timeout = %-4d }, patch_coverage = %10s, measurements = %-6d }\n" \
         "$bm"                       \
         "$success_refactor"         \
         "$failure_refactor"         \
         "$success_benchmark"        \
         "$failure_generic"          \
         "$failure_timeout"          \
-        "$benchmark_patch_coverage"
+        "$benchmark_patch_coverage" \
+        "$metrics_count"
 done
 
